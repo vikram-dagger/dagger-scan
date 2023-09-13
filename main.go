aa44fa80d7294ad7c9e419717dc994a19b6efc4a
Vikram Vaswani <vikram@dagger.io>
2023-09-14 03:05:44 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+)
+
+type Scan struct{}
+
+func (ctr *Container) Snyk(ctx context.Context, snykToken *Secret) (*Container, error) {
+
+	c := ctr.
+		WithWorkdir("/tmp").
+		WithExec([]string{"curl", "https://static.snyk.io/cli/latest/snyk-linux", "-o", "snyk"}).
+		WithExec([]string{"chmod", "+x", "snyk"}).
+		WithExec([]string{"mv", "./snyk", "/usr/local/bin"}).
+		WithWorkdir("/src").
+		WithSecretVariable("SNYK_TOKEN", snykToken).
+		WithExec([]string{"snyk", "monitor", "--all-projects", "--org=5e86b410-1a77-462a-a352-901a216fc3a6"})
+
+	return c, nil
+}
